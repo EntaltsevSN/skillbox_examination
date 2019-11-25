c2eac427497c6246700a1bd3435f61fef4b6b653
EntaltsevSN <stanislav@lotus-studio.co>
2019-11-25 08:25:09 +0300

    starter package

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+let path = require('path');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'bundle'),
+    filename: 'bundle.js',
+    publicPath: 'bundle'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ] 
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+  ]
+};
+
+module.exports = config;
